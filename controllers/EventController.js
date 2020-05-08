3646306a07df73be1710cf736f17e3ec7b96266e
Razmik <razmikbortorian188@gmail.com>
2020-05-08 19:29:32 +0400

    Update Create event

--- controllers/EventController.js
+++ controllers/EventController.js
@@ -4,32 +4,33 @@ const {User, Events, Employees, AttachedEmployees} = require('../models');
 
 async function getEvent(req, res){
 
-    const events = await User.findAll({
+    const events = await Events.findAll({
+        attributes: ['id', 'eventName','eventLogo', 'date', 'startTime', 'endTime'],
+        include:[
+            {
+                attributes: ['id'],
+                model:AttachedEmployees,
+                as:'attachedEmployees',
+                include:[
+                    {
+                        attributes :['id', 'firstName', 'lastName', 'status', 'role'],
+                        model:User,
+                        as:'Company',
+                        where:{
+                            role:2
+                        },
+                        include : [
+                            {
+                                attributes :['companyName', 'JobTitle', 'profileImg', 'companyImg', 'videoUrl'],
+                                model:Employees,
+                                as:'employee'
+                            }
+                        ],
+                    }
+                ]
+            },
 
-     include:[
-         {
-             model:AttachedEmployees,
-             as:'attachedEmployees',
-             include:[
-                 {
-                     attributes :['firstName', 'lastName', 'status', 'role'],
-                     model:User,
-                     as:'Company',
-                     where:{
-                         role:2
-                     },
-                     include : [
-                         {
-                             attributes :['companyName', 'JobTitle', 'profileImg', 'companyImg', 'videoUrl'],
-                             model:Employees,
-                             as:'employee'
-                         }
-                     ],
-                 }
-             ]
-         }
-     ],
-
+        ]
     });
 
     return  res.status(httpStatus.OK).json({
