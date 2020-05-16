4b0d7fe33ec215736f6920d9c41cd4c65c911763
Razmik <razmikbortorian188@gmail.com>
2020-05-16 15:12:11 +0400

    Some change

---
+++ models/favorit.js
@@ -0,0 +1,13 @@
+
+module.exports = (sequelize, DataTypes) => {
+
+  const SupportingDocs = sequelize.define('Favorits', {
+    candidateId: DataTypes.INTEGER,
+    employeeId: DataTypes.INTEGER,
+  }, {});
+
+  SupportingDocs.associate = function(models) {
+  };
+
+  return SupportingDocs;
+};
