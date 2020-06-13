16bd612d04858aaac1f603c2f1f691bd3ade62c6
prmlee <promotionlee.bs@gmail.com>
2020-06-14 00:24:55 +0700

    add timezoneOffset to employeeSetting.

--- migrations/20200613172317-add-timezone-to-employeeSettings.js
+++ migrations/20200613172317-add-timezone-to-employeeSettings.js
@@ -4,7 +4,7 @@ module.exports = {
   up: function(queryInterface, Sequelize) {
     // logic for transforming into the new state
     return queryInterface.addColumn(
-      'Events',
+      'employeeSettings',
       'timezoneOffset',
       {
         type: Sequelize.INTEGER,
@@ -17,7 +17,7 @@ module.exports = {
   down: function(queryInterface, Sequelize) {
     // logic for reverting the changes
     return queryInterface.removeColumn(
-      'Events',
+      'employeeSettings',
       'timezoneOffset'
     );
   }
