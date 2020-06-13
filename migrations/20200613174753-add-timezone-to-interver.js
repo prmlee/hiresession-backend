1706c122f2099e234dc91cacbfa55c22d11b69d8
CuongHuynh <cuonghuynhvan@gmail.com>
2020-06-14 00:59:58 +0700

    add timezoneOffset for interview. log for zoom.

---
+++ migrations/20200613174753-add-timezone-to-interver.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    // logic for transforming into the new state
+    return queryInterface.addColumn(
+      'Interviews',
+      'timezoneOffset',
+      {
+        type: Sequelize.INTEGER,
+        defaultValue: 240, // EST TIME
+      }
+    );
+
+  },
+
+  down: function(queryInterface, Sequelize) {
+    // logic for reverting the changes
+    return queryInterface.removeColumn(
+      'Interviews',
+      'timezoneOffset'
+    );
+  }
+};
