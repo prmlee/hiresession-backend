013044b3f2ab69e1e6ced95f839e3ea4db17d5d1
prmlee <promotionlee.bs@gmail.com>
2020-07-10 04:27:32 +0900

    implement auto backup step2

--- backup/cron.js
+++ backup/cron.js
@@ -1,12 +1,12 @@
 const CronJob = require('cron').CronJob;
-const Cron = require('./backup.js');
+const db_dumper = require('./backup.js');
 
 
 // AutoBackUp every week (at 00:00 on Sunday)
 new CronJob(
   '0 * *  * *',
   function() {
-    Cron.dbAutoBackUp();
+    db_dumper();
   },
   null,
   true,
