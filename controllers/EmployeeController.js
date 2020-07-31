18c0d40b0df2d9ac46088596f990fe09580c802c
Senior Mobile Developer <successmichal@gmail.com>
2020-07-31 12:42:11 -0400

    set unlimit on search candidates

--- controllers/EmployeeController.js
+++ controllers/EmployeeController.js
@@ -545,7 +545,7 @@ async function checkEmployeeSettingsFull(eventId,employeeId){
 }
 
 async function searchCandidates(req, res) {
-  const limit = 100;
+  //const limit = 100;
   console.log(req.body);
   var searchCondition = {};
 
@@ -581,7 +581,6 @@ async function searchCandidates(req, res) {
         where: searchCondition
       },
     ],
-    limit,
     where: {
       role: 1,
       status: 1
