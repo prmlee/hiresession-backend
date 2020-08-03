1d128e62c1d2de608d1080b347fdbb163d76ffe2
Senior Mobile Developer <successmichal@gmail.com>
2020-08-03 05:37:34 -0400

    add share column to candidate table and remove shareResume column from interviews table.

--- models/interview.js
+++ models/interview.js
@@ -29,7 +29,6 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       defaultValue: 'EST',
     },
-    shareResume: DataTypes.STRING,
   }, {});
 
   Interviews.associate = function(models) {
