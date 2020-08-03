52d74f7d80452a3010d995a58c584a3115b08952
Senior Mobile Developer <successmichal@gmail.com>
2020-08-03 06:30:05 -0400

    fixed share

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -137,7 +137,7 @@ async function candidateRegister(req, res) {
         zipCode: req.body.zipCode || '',
         specialNeeds: req.body.specialNeeds || '',
         aboutMe: req.body.aboutMe || '',
-        share: req.body.share || 0,
+        share: req.body.share,
         resume,
         resumeFileName,
         profileImg,
