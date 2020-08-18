83c6689172019bbd92506a9f978d08097f7db9a2
saso124 <eyeway124@yandex.com>
2020-08-18 15:20:17 -0400

    implement infosession step8

---
+++ migrations/20200818111516-add-employsettings-joinUrl.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    // logic for transforming into the new state
+    return queryInterface.addColumn(
+      'employeeSettings',
+      'joinUrl',
+      {
+        type: Sequelize.STRING,
+        defaultValue:''
+      },
+    );
+
+  },
+
+  down: function(queryInterface, Sequelize) {
+    // logic for reverting the changes
+    return queryInterface.removeColumn(
+      'employeeSettings',
+      'joinUrl'
+    );
+  }
+};
