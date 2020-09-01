8d4093de7b14462cfa714395c96c49d4bb146028
prmlee <promotionlee.bs@gmail.com>
2020-09-01 09:58:00 +0000

    add military field to candidates table.

---
+++ migrations/20200901030515-add-candidate-isYouMilitary.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    // logic for transforming into the new state
+    return queryInterface.addColumn(
+      'Candidates',
+      'isYouMilitary',
+      {
+        type: Sequelize.INTEGER,
+        defaultValue:0
+      },
+    );
+
+  },
+
+  down: function(queryInterface, Sequelize) {
+    // logic for reverting the changes
+    return queryInterface.removeColumn(
+      'Candidates',
+      'isYouMilitary'
+    );
+  }
+};
