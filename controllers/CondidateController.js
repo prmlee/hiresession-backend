2a8752072fdee148cfdfa4b35275df739e84c98b
prmlee <promotionlee.bs@gmail.com>
2020-10-09 10:11:29 -0700

    step fix 10

--- controllers/CondidateController.js
+++ controllers/CondidateController.js
@@ -573,9 +573,9 @@ async function getSingleEmployee(req, res) {
   let date = moment(new Date()).format('YYYY-MM-DD');
   let times = [];
   console.log("step1",eventId);
-  date = await getFirstDate(req.body.employeeId, eventId);
+  date = await getFirstDate(req.body.employeeId, req.body.eventId);
   console.log("step2",date);
-  times = await getTimes(req.body.employeeId, date, eventId);
+  times = await getTimes(req.body.employeeId, date, req.body.eventId);
   console.log("step3",times);
   return res.status(httpStatus.OK).json({
     success: true,
