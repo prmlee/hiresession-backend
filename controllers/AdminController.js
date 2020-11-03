64e980ea87ff590c44821979610869e3d4e6c73a
saso124 <eyeway124@yandex.com>
2020-11-03 08:20:39 +0000

    fix bug eventlist.

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -1023,7 +1023,7 @@ async function getCandidates(req, res) {
     userCondition.createdAt = tempObj;
   }
   
-  if(req.body.eventList.length !=0)
+  if(req.body.eventList!=null && req.body.eventList.length !=0)
   {
     eventCondition.id = {[Op.in]:req.body.eventList};
   }
