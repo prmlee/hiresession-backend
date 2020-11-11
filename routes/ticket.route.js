606fd26117bcd2a606634c06cc6b92ba3a808249
prmlee <promotionlee.bs@gmail.com>
2020-11-11 17:15:37 +0000

    implement payment step1

---
+++ routes/ticket.route.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const ticketController = require('../controllers/TicketController');
+const { isLoggedAdmin } = require('../middlewares/auth');
+
+const router = express.Router();
+
+router
+    .route('/addTicketType')
+    .post(
+        isLoggedAdmin,
+        ticketController.addTicketType
+    );
+
+router
+    .route('/getTicketTypes')
+    .post(
+        isLoggedAdmin,
+        ticketController.getTicketTypes
+    );
+
+router
+    .route('/deleteTicketType')
+    .post(
+        isLoggedAdmin,
+        ticketController.deleteTicketType
+    );
+
+router
+    .route('/updateTicketType')
+    .post(
+        isLoggedAdmin,
+        ticketController.updateTicketType
+    );
+
+module.exports = router;
