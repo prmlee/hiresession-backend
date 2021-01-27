e0ae70896ff892754bc29f75e43a275aafa857a7
prmlee <promotionlee.bs@gmail.com>
2021-01-27 17:35:09 +0000

    subscription step4

--- controllers/ZoomController.js
+++ controllers/ZoomController.js
@@ -3,6 +3,7 @@ const httpStatus = require('http-status');
 async function subscription(req,res)
 {
 	console.log("subscription",req.body);
+	console.log("participant",req.body.payload.object.participant);
 	return res.status(httpStatus.OK).json({
 		success: true,
 	});
