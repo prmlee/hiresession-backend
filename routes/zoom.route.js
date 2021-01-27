d134f9242f69838efa472ed6c299ed611b3a93e7
prmlee <promotionlee.bs@gmail.com>
2021-01-27 17:02:10 +0000

    subscription step2

--- routes/zoom.route.js
+++ routes/zoom.route.js
@@ -1,25 +1,9 @@
 const express = require('express');
 const {check} = require('express-validator/check');
-const adminController = require('../controllers/AdminController');
 const zoomController = require('../controllers/ZoomController');
-const { isLoggedAdmin } = require('../middlewares/auth');
 
 const router = express.Router();
 
-router
-    .route('/createEvent')
-    .post(
-        isLoggedAdmin,
-        adminController.createEvent
-    );
-
-
-router
-    .route('/updateEvent')
-    .patch(
-        isLoggedAdmin,
-        adminController.updateEvent
-	);
 	
 router
 	.route('/subscription')
