63e479d4354eb9db7d16e72cfcf5d3763cb67142
prmlee <promotionlee.bs@gmail.com>
2021-01-29 17:11:05 +0000

    fix subscription issue1

--- controllers/ZoomController.js
+++ controllers/ZoomController.js
@@ -1,9 +1,49 @@
 const httpStatus = require('http-status');
+const moment = require('moment');
+const { User, Interviews } = require('../models');
+const prevDate = moment().subtract(1, 'day').format('YYYY-MM-DD');
+const nextDate = moment().add(1, 'day').format('YYYY-MM-DD');
+const today = moment().format('YYYY-MM-DD');
 
 async function subscription(req,res)
 {
-	console.log("subscription",req.body);
-	console.log("participant",req.body.payload.object.participant);
+	
+
+	try{
+		const type = req.body.event;
+		const payload = req.body.payload;
+		const infoObj = req.body.payload.object;
+
+		console.log("subscription type",type);
+		console.log("subscription payload",type);
+
+		if(infoObj.join_time == payload.start_time)
+		{
+			return res.status(httpStatus.OK).json({
+				success: true,
+			});	
+		}
+
+		if(type == "meeting.participant_joined")
+		{
+			const interviewList = await Interviews.findAll({
+				where: {
+					[Op.or]: [
+						{ date: prevDate },
+						{ date: today },
+						{ date: nextDate },
+					],
+					'joinUrl': `https://zoom.us/j/${payload.id}`
+				},
+			});
+
+			console.log("interviewList",interviewList);
+		}
+	}
+	catch(e)
+	{
+
+	}
 	return res.status(httpStatus.OK).json({
 		success: true,
 	});
